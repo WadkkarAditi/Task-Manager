77bb529b52df19d1fc0c8eddc87842fd29643f03
shash <shashankjagtap1809@gmail.com>
2025-11-16 19:20:09 +0530

    Admin page error resolved

--- frontend/Task Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task Manager/src/pages/Admin/Dashboard.jsx
@@ -6,6 +6,9 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import moment from 'moment';
+import { addThousandsSeparator } from "../../utils/helper";
+import InfoCard from "../../Cards/InfoCard";
 
 const Dashboard= () =>
 {
@@ -15,7 +18,7 @@ const {user}= useContext(UserContext);
 
 const navigate = useNavigate();
 
-const [dashboard, setDashboardData]= useState(null);
+const [dashboardData, setDashboardData]= useState(null);
 const [pieChartData, setPieChartData]= useState([]);
 const [barChartData, setBarChartData]= useState([]);
 
@@ -27,12 +30,12 @@ const getDashboardData = async() =>
  );
  if (response.data)
  {
-  setDashboardData(response.data);
+    setDashboardData(response.data);
  }
  }
  catch(error)
  {
- console.error("Error fetching user data:", error); 
+    console.error("Error fetching user data:", error); 
  }
 };
 
@@ -43,7 +46,29 @@ useEffect(()=>
  return ()=> {};
 }, []);
 
-return <DashboardLayout activeMenu="Dashboard"> Dashboard </DashboardLayout>;
+return (<DashboardLayout activeMenu="DashBoard">
+    <div className="card my-5">
+        <div>
+            <div className="col-span-3">
+                <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+                <p className="text-xs md:text-{13px} text-gray-400 mt-1.5">
+                    {moment().format("dddd Do MM YYYY")}
+                </p>
+            </div>
+        </div>
 
+         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+            <InfoCard
+                 label="Total Tasks"
+                 value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.All || 0
+                )}
+                color="bg-primary"
+            />
+        </div>
+    </div>
+</DashboardLayout>
+);
 };
+
 export default Dashboard;
